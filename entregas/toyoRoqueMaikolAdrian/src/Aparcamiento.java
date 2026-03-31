89f04fe5f339bd6f6d7e353c6ac76e3817f679f1
Maikoledu1303 <maikol.toyo@alumnos.uneatlantico.es>
2026-03-31 23:32:55 +0000

    feature: implementar vista privada de clase aparcamiento
    he implementado los metodos principales de la clase aparcamiento, faltarian por implementar los metodos registrarEntrada y consultarEstado

--- entregas/toyoRoqueMaikolAdrian/src/Aparcamiento.java
+++ entregas/toyoRoqueMaikolAdrian/src/Aparcamiento.java
@@ -1,11 +1,44 @@
 class Aparcamiento{
-    public Aparcamiento(int numeroDePlazas){};
-    public Aparcamiento(){};
+    private Plaza[] listadoDePlazas;
 
-    public void agregarPlaza(int numeroDePlaza, Plaza plaza){};
-    public Plaza mostrarPlaza(int numeroDePlaza){};
-    public int expedirTarifa(int horaDeEntrada, int horaDeSalida, Plaza plaza){};
-    public int consultarEspaciosDisponibles(){};
+    public Aparcamiento(int numeroDePlazas){
+        this.listadoDePlazas = new Plaza[numeroDePlazas];
+    };
+
+    public Aparcamiento(){
+        this.listadoDePlazas = new Plaza[10];
+    };
+
+    public void agregarPlaza(int numeroDePlaza, Plaza plaza){
+        this.listadoDePlazas[numeroDePlaza] = plaza;
+    };
+
+    public Plaza mostrarPlaza(int numeroDePlaza){
+        return this.listadoDePlazas[numeroDePlaza];
+    }
+
+    public int expedirTarifa(int horaDeEntrada, int horaDeSalida, Plaza plaza){
+        String tipo = plaza.mostrarTipo();
+        int horasAparcado = horaDeSalida - horaDeEntrada;
+        
+        return switch (tipo) {
+            case "Electrica" -> horasAparcado * 3;
+            case "Normal"    -> horasAparcado * 2;
+            case "Moto"      -> horasAparcado * 1;
+            default          -> 0;
+        }; 
+    }
+
+    public int consultarEspaciosDisponibles(){
+        int disponibles = 0;
+        for (Plaza plaza : this.listadoDePlazas) {
+            if (plaza == null) {
+                disponibles++;
+            }
+        }
+        return disponibles;
+    }
+
     public void registrarEntrada(Plaza plaza, int horaDeEntrada){};
     public String consultarEstado(Plaza plaza){};
 }
--- entregas/toyoRoqueMaikolAdrian/src/README.md
+++ entregas/toyoRoqueMaikolAdrian/src/README.md
@@ -1,3 +1,7 @@
 # Examen Parcial
 
-En este directorio se llevo a cabo el desarrollo del examen parcial, mas especificamente la vista publica, comenzaré agregando la vista publicas desarrolladas en presencial para luego implmentar la vista privada. En principio podría encontrar problemas debido a algún error al crear las vistas publicas previamente. 
+En este directorio se llevará a cabo el desarrollo del examen parcial, mas especificamente la vista pública, comenzaré agregando la vista públicas desarrolladas en presencial para luego implmentar la vista privada. En principio podría encontrar problemas debido a algún error al crear las vistas públicas previamente. 
+
+## Implementacion de vista privada
+
+Habiendo implementado la vista privada de las clases auxiliares comenzaré a implementar la vista privada de la clase aparcamiento, lo lógico sería crear un array de plazas tomando el número de plazas que le paso como parámetro para crear el aparcamiento y de esta forma tener acceso a cada una de las plazas de forma independiente. El reto en este caso sería implementar los métodos de esta clase lo mejor posible sin poder modificar la vista pública anteriormente creada.
